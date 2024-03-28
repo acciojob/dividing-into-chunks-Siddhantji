7d347df2e7cdc670656a1735e448c512b4c519de
Siddhant Teotia <91278902+Siddhantji@users.noreply.github.com>
2024-03-28 15:38:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
 const arr = [1, 2, 3, 4, 1, 0, 2, 2];
 const divide = (arr, n) => {
 
-	cont result=[];
+	cont r=[];
 	let sub=[];
 	for(const num of arr){
 		if(sub.reduce((acc,val)=> acc+val,0)+num<= n){
 			sub.push(num);
 		}
 		else{
-			result.push(sub);
+			r.push(sub);
 			sub.push(num);
 		}
       }
 	if (sub.length > 0) {
-    result.push(sub);
+    r.push(sub);
 }
 
-	return result;
+	return r;
 };
 const n = prompt("Enter n: ");
 alert(JSON.stringify(divide(arr, n)));
